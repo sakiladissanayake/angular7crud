bf4e262ab6395b910f825077eee143a65ad14833
sakiladissanayake <sakilad@gmail.com>
2018-12-25 12:04:24 +0530

    add validation part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { from } from 'rxjs';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SlimLoadingBarModule
+    SlimLoadingBarModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gst-add/gst-add.component.ts
+++ src/app/gst-add/gst-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-gst-add',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gst-add.component.css']
 })
 export class GstAddComponent implements OnInit {
+  angForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+   }
 
-  constructor() { }
+   createForm() {
+    this.angForm = this.fb.group({
+      person_name: ['', Validators.required ],
+      business_name: ['', Validators.required ],
+      business_gst_number: ['', Validators.required ]
+    });
+   }
 
   ngOnInit() {
   }
